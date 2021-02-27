8856f6a430b5ce3c18929a6335f9e103ea163dbc
GoogleSites <discodude1234@gmail.com>
2021-02-26 19:56:47 -0500

    first remove

--- index.js
+++ index.js
@@ -9,39 +9,10 @@ const EventEmitter = require('events').EventEmitter
 const pluginLoader = require('./lib/plugin_loader')
 const supportFeature = require('./lib/supportFeature')
 const plugins = {
-  bed: require('./lib/plugins/bed'),
-  title: require('./lib/plugins/title'),
-  block_actions: require('./lib/plugins/block_actions'),
-  blocks: require('./lib/plugins/blocks'),
-  book: require('./lib/plugins/book'),
-  boss_bar: require('./lib/plugins/boss_bar'),
   chat: require('./lib/plugins/chat'),
-  chest: require('./lib/plugins/chest'),
-  command_block: require('./lib/plugins/command_block'),
-  craft: require('./lib/plugins/craft'),
-  creative: require('./lib/plugins/creative'),
-  digging: require('./lib/plugins/digging'),
-  enchantment_table: require('./lib/plugins/enchantment_table'),
   entities: require('./lib/plugins/entities'),
-  experience: require('./lib/plugins/experience'),
-  fishing: require('./lib/plugins/fishing'),
-  furnace: require('./lib/plugins/furnace'),
   game: require('./lib/plugins/game'),
-  health: require('./lib/plugins/health'),
-  inventory: require('./lib/plugins/inventory'),
-  kick: require('./lib/plugins/kick'),
-  physics: require('./lib/plugins/physics'),
-  place_block: require('./lib/plugins/place_block'),
-  rain: require('./lib/plugins/rain'),
-  ray_trace: require('./lib/plugins/ray_trace'),
-  scoreboard: require('./lib/plugins/scoreboard'),
-  settings: require('./lib/plugins/settings'),
-  simple_inventory: require('./lib/plugins/simple_inventory'),
-  sound: require('./lib/plugins/sound'),
-  spawn_point: require('./lib/plugins/spawn_point'),
-  tablist: require('./lib/plugins/tablist'),
-  time: require('./lib/plugins/time'),
-  villager: require('./lib/plugins/villager')
+  kick: require('./lib/plugins/kick')
 }
 
 const supportedVersions = require('./lib/version').supportedVersions
